71eb342aed1ab73568e872c0f31ddb6fa2863e6c
Bimal Parajuli <bimalparajuli@Bimals-MacBook-Pro.local>
2017-09-21 12:33:07 -0700

    Print numbers 1 to n

---
+++ com/codingTest/java/PrintWithoutLoop.java
@@ -0,0 +1,19 @@
+package com.codingTest.java;
+
+public class PrintWithoutLoop {
+
+	public static void main(String[] args) {
+		print(10);
+		// print 1 to 10 without loop
+	}
+	public static int i = 1;
+	private static void print(int n) {
+		if(i <= n){
+			System.out.print(" "+i++);
+			print(n);
+		}
+		
+	}
+	
+
+}
